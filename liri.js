dbcd770353d843ace90304ab743001cf704d164e
Huan Nguyen <huancal1984@gmail.com>
2019-11-22 00:13:47 -0800

    spotify keys added

--- liri.js
+++ liri.js
@@ -1,5 +1,20 @@
 var keys = require('./keys')
 
+const axios = require("axios")
+
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+
+// concert-this command
+
+const url = "https: //rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp"
+
+// const artist = () => {
+//     const arguments = process.argv;
+// }
+
+
+axios.get(url).then(res =>
+    console.log(res.venue));
